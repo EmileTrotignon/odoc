bcffe802197323964ecf5d309b6d44b16af95c11
Emile Trotignon <emile@tarides.com>
2023-06-12 13:16:45 +0200

    Allows generating json_display from parts.

--- src/search/json_display.ml
+++ src/search/json_display.ml
@@ -1,11 +1,4 @@
-let json_of_id x =
-  `Array
-    (x |> Odoc_model.Paths.Identifier.fullname
-    |> List.map (fun str -> `String str))
-
-let json_of_doc (doc : Odoc_model.Comment.docs) =
-  let html = Render.html_of_doc doc in
-  `String (Format.asprintf "%a" (Tyxml.Html.pp_elt ()) html)
+let json_of_namelist li = `Array (List.map (fun str -> `String str) li)
 
 let display_expression_rhs args res =
   let open Odoc_model.Lang in
@@ -52,47 +45,74 @@ let display_constructor_type args res =
       let res = Render.text_of_type res in
       fields ^ " -> " ^ res
 
+let typedecl_rhs td =
+  let segments = String.split_on_char '=' td.Entry.txt in
+  if List.length segments > 1 then
+    segments |> List.tl |> String.concat "=" |> String.trim |> ( ^ ) " = "
+    |> Option.some
+  else None
+
+let constructor_rhs Entry.{ args; res } =
+  " : " ^ display_constructor_type args res
+
+let field_rhs Entry.{ mutable_ = _; type_; parent_type = _ } =
+  " : " ^ Render.text_of_type type_
+
+let rhs_of_kind extra =
+  let open Entry in
+  match extra with
+  | TypeDecl td -> typedecl_rhs td
+  | Constructor cons -> Some (constructor_rhs cons)
+  | Field field -> Some (field_rhs field)
+  | Exception { args; res } -> Some (display_expression_rhs args res)
+  | Value { value = _; type_ } -> Some (" : " ^ Render.text_of_type type_)
+  | Module | Doc _ | Class_type _ | Method _ | Class _ | TypeExtension _
+  | ExtensionConstructor _ | ModuleType ->
+      None
+
+let of_strings ~id ~url ~doc ~kind ~rhs : Odoc_html.Json.json =
+  let rhs =
+    match rhs with None -> [] | Some rhs -> [ ("rhs", `String rhs) ]
+  in
+  let j_url = `String url in
+  let j_id = json_of_namelist id in
+  let doc = `String doc in
+  let kind = `String kind in
+  `Object (rhs @ [ ("id", j_id); ("url", j_url); ("kind", kind); ("doc", doc) ])
+
 let of_entry ({ id; doc; extra } : Entry.t) : Odoc_html.Json.json =
-  let j_url = `String (Render.url id) in
-  let j_id = json_of_id id in
-  let doc = json_of_doc doc in
+  let url = Render.url id in
+  let id = Odoc_model.Paths.Identifier.fullname id in
+  let doc =
+    doc |> Render.html_of_doc |> Format.asprintf "%a" (Tyxml.Html.pp_elt ())
+  in
   let kind =
-    `String
-      (match extra with
-      | TypeDecl _ -> "type"
-      | Module -> "module"
-      | Value _ -> "val"
-      | Doc _ -> "doc"
-      | Exception _ -> "exn"
-      | Class_type _ -> "class type"
-      | Method _ -> "method"
-      | Class _ -> "class"
-      | TypeExtension _ ->
-          (* TODO: include type_path and type_params *)
-          "type ext"
-      | ExtensionConstructor _ -> "extension constructor"
-      | ModuleType -> "module type"
-      | Constructor _ -> "constructor"
-      | Field _ -> "field")
+    match extra with
+    | TypeDecl _ -> "type"
+    | Module -> "module"
+    | Value _ -> "val"
+    | Doc _ -> "doc"
+    | Exception _ -> "exn"
+    | Class_type _ -> "class type"
+    | Method _ -> "method"
+    | Class _ -> "class"
+    | TypeExtension _ ->
+        (* TODO: include type_path and type_params *)
+        "type ext"
+    | ExtensionConstructor _ -> "extension constructor"
+    | ModuleType -> "module type"
+    | Constructor _ -> "constructor"
+    | Field _ -> "field"
   in
   let rhs =
     match extra with
-    | TypeDecl { canonical = _; equation = _; representation = _; txt } ->
-        let segments = String.split_on_char '=' txt in
-        if List.length segments > 1 then
-          segments |> List.tl |> String.concat "=" |> String.trim |> ( ^ ) " = "
-          |> Option.some
-        else None
-    | Constructor { args; res } ->
-        Some (" : " ^ display_constructor_type args res)
-    | Field { mutable_ = _; type_; parent_type = _ } ->
-        Some (" : " ^ Render.text_of_type type_)
+    | TypeDecl td -> typedecl_rhs td
+    | Constructor cons -> Some (constructor_rhs cons)
+    | Field field -> Some (field_rhs field)
     | Exception { args; res } -> Some (display_expression_rhs args res)
     | Value { value = _; type_ } -> Some (" : " ^ Render.text_of_type type_)
     | Module | Doc _ | Class_type _ | Method _ | Class _ | TypeExtension _
     | ExtensionConstructor _ | ModuleType ->
         None
   in
-  `Object
-    ((match rhs with None -> [] | Some rhs -> [ ("rhs", `String rhs) ])
-    @ [ ("id", j_id); ("url", j_url); ("kind", kind); ("doc", doc) ])
+  of_strings ~id ~url ~doc ~kind ~rhs
